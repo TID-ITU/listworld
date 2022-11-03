b88fcdf13dba1fbea30f23d894b776cefb7c2f0e
bwesth <bjwe@itu.dk>
2022-11-03 21:37:13 +0100

    input in list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react';
 import './ProductList.css'
 
 const List = () => {
-    // const [list, setList] = useState([])
+    const [input, setInput] = useState([])
 
     // useEffect(() => {
     //     if (list.length === 0) {
 
     //     }
     // },)
+
+    function handleChange(event) {
+        setInput(event.target.value)
+    }
+
     return (
+        <>
+        <input type="text" value={input} onChange={handleChange} />
         <ul>
             
         </ul>
+        </>
     )
 }
 
