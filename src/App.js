df665db048fb693ac931ba45c559fec805057134
bwesth <bjwe@itu.dk>
2022-11-03 11:22:21 +0100

    more parse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import Person from "./components/ProductList/Person";
 import ProductList from "./components/ProductList/ProductList";
 
 function App() {
+
+  // async function getWish() {
+  //   const query = new Parse.Query("Wish")
+  //   // query.includeAll()
+  //   try {
+  //     const result = await query.get()
+  //     console.log(result)
+      
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  // getWish()
   return (
     <div className="App">
-      <ProductList />
+      {/* <ProductList /> */}
+      <Person />
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Parse from 'parse';
+
+Parse.initialize("yQQzmBNtqIXWZ3hXkpzDbT5TEX8CxQ6MTBps5XAV", "35JNgEsLV9wyypSnL8Qfg2sUCo4b0DLIy2ugTiUJ");
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+Parse.serverURL = PARSE_HOST_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
